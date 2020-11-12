5f4af599b4a8d36747722ef30e1145c5565db5cd
Adarsh Verma <adarshverma0603@gmail.com>
2020-11-12 22:33:59 +0530

    Added graph download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
 @Component({
   selector: 'app-root',
@@ -7,7 +7,9 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent implements OnInit {
   chart;
+  label: string = '#no of votes';
   type: string;
+  @ViewChild('chartimage') chartcanvas: ElementRef;
   setGraph(graphType: string) {
     this.type = graphType;
     this.drawGraph();
@@ -21,7 +23,7 @@ export class AppComponent implements OnInit {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [
           {
-            label: '# of Votes',
+            label: this.label,
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
@@ -57,4 +59,14 @@ export class AppComponent implements OnInit {
     });
   }
   ngOnInit(): void {}
+  downloadImage() {
+    var image = this.chartcanvas.nativeElement
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream'); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+    var link = document.getElementById('link');
+    link.setAttribute('download', `${this.label}(${this.type}).png`);
+    link.setAttribute('href', image);
+    link.click();
+  }
 }
